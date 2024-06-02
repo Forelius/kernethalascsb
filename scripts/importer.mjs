b0d0476d510ae7d37161e8a323eccb7171a6784a
Forelius <140267826+Forelius@users.noreply.github.com>
2024-06-02 05:12:18 -0700

    Added combat round and turn logging to chat. Made personal goals into a single field and converted second personal goal to page 1 notes.

--- scripts/importer.mjs
+++ scripts/importer.mjs
@@ -7,8 +7,8 @@ export class ImportMonsterApp extends FormApplication {
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
             title: 'Import Monster',
-            width: 600,
-            height: 300,
+            width: 800,
+            height: 450,
             template: `modules/kernethalascsb/templates/import-dialog.hbs`
         });
     }
@@ -28,29 +28,7 @@ export class ImportMonsterApp extends FormApplication {
 
     async loadContent(pdfText) {
         const self = this
-        console.log("Ker Nethalas CSB:", pdfText);
-    //    if (file) {
-    //        let zip = await JSZip.loadAsync(file)
-    //        let key = Object.keys(zip.files)[0]
-    //        let targetFile = zip.files[key]
-    //        let jsonData = await targetFile.async("string")
-    //        let data = JSON.parse(jsonData)
-
-    //        let geometryData = data.data.geometry
-    //        let dataNodes = data.state.document.nodes
-    //        let doorIDs = self.getDoorIDs(dataNodes)
-    //        let stairIDs = self.getStairIDs(dataNodes)
-    //        let lightData = self.getLights(dataNodes)
-    //        let doc = dataNodes.document
-    //        let gridCellSize = dataNodes[doc.children[0]].grid.cellDiameter
-
-    //        self.updateButtonStatus(game.i18n.localize("DSI.import.parsingfile"), true)
-    //        let offsetData = await self.parseShapes(geometryData, doorIDs, stairIDs, gridCellSize, offsetX, offsetY)
-    //        await self.processLights(lightData, offsetData, gridCellSize)
-    //        self.updateButtonStatus(game.i18n.localize("DSI.import.finished"))
-    //    } else {
-    //        $('#dsimportErrorMessage').html(game.i18n.localize("DSI.import.error.noFile")).addClass("visible")
-    //        self.updateButtonStatus(game.i18n.localize("DSI.import.import"))
-    //    }
+        let lines = pdfText.split('\n');
+        console.log("Ker Nethalas CSB:", lines);
     }
 }
--- scripts/setup.js
+++ scripts/setup.js
@@ -15,14 +15,37 @@ Hooks.on("ready", function () {
 	//return dialog.render(true);
 });
 
-Hooks.on('renderSidebarTab', async function (sidebar, html) {
-    if (sidebar._element[0].id === 'actors' && game.user.isGM) {
-		console.log('Ker Nethalas CSB: renderSidebarTab', sidebar._element[0].id);
-        let button = $(`<div class='header-actions action-buttons flexrow'><button><i class='fas fa-file-import'></i>Import Monster</button></div>`)
-        button.on('click', async () => {
-            let dialog = new ImportMonsterApp();
-            return dialog.render(true);
-        })
-        $(html).find('.header-actions').append(button);
-    }
+Hooks.on('updateCombat', (combat, updates) => {
+	let msg = "";
+	let combatant = combat.turns[combat.turn];
+	// If round change...
+	if (combat.previous && combat.previous.round !== combat.round) {
+		msg = `<span style="font-size:16px; font-weight:800;">Round ${updates.round}</span><hr>`;
+	}
+	// If turn change...
+	if (combat.previous && combat.previous.turn !== combat.turn) {
+		let possesive = (combatant.name.substr(combatant.name.length - 1) === "s") ? "'" : "'s";
+		let combatantName = combatant.name + possesive;
+		msg = (msg != null) ? msg + '</br>' : "";
+		msg += `${combatantName} turn`
+	}
+	//console.log('updateCombat', combat, updates, msg);
+	if (msg !== null) {
+		msg = `<div>${msg}</div>`;
+		ChatMessage.create({ content: msg, whisper: [game.user.id] });
+	}
+	//if (updates.active) {
+		//new CONFIG.combatTrackerDock.CombatDock(combat).render(true);
+	//}
 });
+//Hooks.on('renderSidebarTab', async function (sidebar, html) {
+//    if (sidebar._element[0].id === 'actors' && game.user.isGM) {
+//		console.log('Ker Nethalas CSB: renderSidebarTab', sidebar._element[0].id);
+//        let button = $(`<div class='header-actions action-buttons flexrow'><button><i class='fas fa-file-import'></i>Import Monster</button></div>`)
+//        button.on('click', async () => {
+//            let dialog = new ImportMonsterApp();
+//            return dialog.render(true);
+//        })
+//        $(html).find('.header-actions').append(button);
+//    }
+//});
